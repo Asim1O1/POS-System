10e7fd415dcbbd975c3cb1748c9ed16b532846e7
biplavsitaula <biplav0007@gmail.com>
2025-08-09 23:11:12 +0545

    setup for routing

---
+++ client/src/router/router.js
@@ -0,0 +1,11 @@
+import { createBrowserRouter } from "react-router";
+import App from "@/App.jsx";
+
+const routes = createBrowserRouter([
+  {
+    path: "/",
+    Component: App,
+  },
+]);
+
+export default routes;
